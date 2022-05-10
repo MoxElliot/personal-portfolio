545407737d8ce874802a8ecf44502f3004a27b86
Elliot Erickson <eric0952@gmail.com>
2022-05-09 18:34:20 -0700

    Dummy Links
    Added dummy links, to be replaced later by a nav bar. This is to set up the routes to each 'page'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom"
+
 function App() {
   return (
     <div>
-    Website Goes Here
+    
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/home">Home</Link> | {" "}
+        <Link to="/portfolio">Portfolio</Link> | {" "}
+        <Link to="/about-me">About Me</Link> | {" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <h1>Website Goes Here</h1>
     </div>
   );
 }
