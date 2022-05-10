38bb50ee6a64823a2a52b8e05b7741902d0798b7
Elliot Erickson <eric0952@gmail.com>
2022-05-09 17:05:33 -0700

    File Setup
    Created route pages (Home, About Me, Portfolio, Contact). Created Component folder with NavBar inside. Connected index.js to the browser's URL with BroweserRouter

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 );
 
 
