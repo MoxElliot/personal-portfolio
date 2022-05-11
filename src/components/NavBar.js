fc431ef16b39684a18fcbe31e0172d85f8312ab3
Elliot Erickson <eric0952@gmail.com>
2022-05-10 18:52:54 -0700

    Links Collapse
    Set Navbar expand='sm' to collaps links on a small screen

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom"
-import { Navbar, Nav } from "react-bootstrap";
-
+import { Nav, Container, Navbar} from "react-bootstrap";
 
 export function NavComponent() {
   return (
-    <Navbar bg="light" expand="lg">
-        <Nav.Link as={Link} to="/home">Home</Nav.Link> | {" "}  
-        <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link> | {" "}
-        <Nav.Link as={Link} to="/about-me">About Me</Nav.Link> | {" "}
-        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+    <Navbar bg="light" expand="sm">
+      <Container>
+        <Navbar.Brand as={Link} to="/home" className="ms-3">Home</Navbar.Brand>  {/* as={Link} is a react-bootstrap render prop which specifies which compoent should be rendered instead of the default (Nav.LinK) */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" >
+          <Nav className="me-auto align-self-end">
+            <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link as={Link} to="/about-me">About Me</Nav.Link> 
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
+ 
