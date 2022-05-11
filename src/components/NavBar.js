8bc123e23f3cd318837f81129114697166f84883
Elliot Erickson <eric0952@gmail.com>
2022-05-11 12:35:51 -0700

    Home Image
    Replaced Home text link with logo image. Resized image to 70rem

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom"
-import { Nav, Container, Navbar} from "react-bootstrap";
+import { Nav, Container, Navbar, Image} from "react-bootstrap";
 
 export function NavComponent() {
   return (
     <Navbar bg="light" expand="sm">
       <Container>
-        <Navbar.Brand as={Link} to="/home" className="ms-3">Home</Navbar.Brand>  {/* as={Link} is a react-bootstrap render prop which specifies which compoent should be rendered instead of the default (Nav.LinK) */}
+        <Navbar.Brand as={Link} to="/home" className="ms-2">
+          <Image height={"70rem"} src={require("./golden-ratio.png")} alt="Home Icon" />
+        </Navbar.Brand>  {/* as={Link} is a react-bootstrap render prop which specifies which compoent should be rendered instead of the default (Nav.LinK) */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" >
-          <Nav className="me-auto align-self-end">
+          <Nav className="ms-auto">
             <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
             <Nav.Link as={Link} to="/about-me">About Me</Nav.Link> 
             <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
