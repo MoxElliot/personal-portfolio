ad31e8019661d8e89d26ff1aebf0ad86d89e5f2c
Elliot Erickson <eric0952@gmail.com>
2022-05-13 12:26:36 -0700

    Nav Functionality
    Added selected functionality to Nav.Links. Variant='tabs' highlights active link. The eventkey option changes the active tab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,41 @@
 import { Link } from "react-router-dom"
-import { Nav, Container, Navbar, Image} from "react-bootstrap";
+import { Nav, Navbar, Image, Container} from "react-bootstrap";
+
 
 export function NavComponent() {
   return (
     <Navbar bg="light" expand="sm">
-      <Container>
-        <Navbar.Brand as={Link} to="/home" className="ms-2">
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/home" className="ms-2 active">
           <Image height={"70rem"} src={require("./golden-ratio.png")} alt="Home Icon" />
         </Navbar.Brand>  {/* as={Link} is a react-bootstrap render prop which specifies which compoent should be rendered instead of the default (Nav.LinK) */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" >
-          <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
-            <Nav.Link as={Link} to="/about-me">About Me</Nav.Link> 
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          <Nav className="ms-auto" variant="tabs">
+            <Nav.Link 
+              as={Link} 
+              to="/portfolio"
+              eventKey="link-1"
+              >
+                Portfolio
+            </Nav.Link>
+            <Nav.Link 
+              as={Link} 
+              to="/about-me"
+              eventKey="link-2"
+              >
+                About Me
+            </Nav.Link> 
+            <Nav.Link 
+              as={Link} 
+              to="/contact"
+              eventKey="link-3"
+              >
+                Contact
+              </Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Container>
+        </Container>
     </Navbar>
   );
 }
