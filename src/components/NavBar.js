2f57e7b5bc2d98b90549ca7a7c8afa88b7fdb989
Elliot Erickson <eric0952@gmail.com>
2022-05-16 09:35:08 -0700

    Home Link
    Added Home link to Nav.Link set and removed the link option from the Navbar.Brand because I counldn't get the link styling to changed when clicked.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,12 +6,18 @@ export function NavBar() {
   return (
     <Navbar bg="light" expand="sm">
       <Container fluid>
-        <Navbar.Brand as={Link} to="/home" className="ms-2 active">
+        <Navbar.Brand className="ms-2">
           <Image height={"70rem"} src={require("../images/golden-ratio.png")} alt="Home Icon" />
         </Navbar.Brand>  {/* as={Link} is a react-bootstrap render prop which specifies which compoent should be rendered instead of the default (Nav.LinK) */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" >
           <Nav className="ms-auto" variant="tabs">
+          <Nav.Link 
+              as={Link} 
+              to="/home"
+              eventKey="link-4"
+              >Home
+            </Nav.Link>
             <Nav.Link 
               as={Link} 
               to="/portfolio"
