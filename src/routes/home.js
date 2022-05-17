45c0732bb6a2625bbe86dac3fff114ec247d36b6
Elliot Erickson <eric0952@gmail.com>
2022-05-17 14:01:29 -0700

    Removed Border
    Removed boarders from Main page

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,8 +4,8 @@ export default function Home() {
     return (
         <main style={{ padding: "1rem"}} >
             <Container>
-                <Row className="border border-dark">  
-                    <Col className="border border-primary m-1 mt-5">
+                <Row>  
+                    <Col className="m-1 mt-5">
                         <h1>
                             Elliot Erickson
                         </h1>
@@ -20,21 +20,21 @@ export default function Home() {
                         </h3>
                         </Container>
                     </Col>
-                    <Col className="border border-primary m-1">
-                    <Image height={"300rem"} src={require("../images/golden-ratio.png")} alt="Logo Icon" />
+                    <Col className="m-1">
+                    <Image height={"300rem"} src={require("../images/golden-ratio.png")} alt="Logo Icon"/>
                     </Col>
                 </Row>
-                <Row className="border border-dark">  
-                    <Col className="border border-primary m-1">
-                        <Image src={require("../images/portfolio-preview.PNG")} className="img-fluid" alt="Portfolio Preview" />
+                <Row>  
+                    <Col className="m-1">
+                        <Image src={require("../images/portfolio-preview.PNG")} fluid alt="Portfolio Preview" />
                     </Col>
-                    <Col className="border border-primary m-1 d-none d-sm-block">
-                        <Image height={"600rem"} src={require("../images/profile-pic.png")} className="img-fluid" alt="Profile Picture" />
+                    <Col className="m-1 d-none d-sm-block">
+                        <Image height={"600rem"} src={require("../images/profile-pic.png")} fluid alt="Profile Picture" />
                     </Col>
                     
                 </Row>
-                <Row className="border border-dark flex-row-reverse">  
-                    <Col className="border border-primary d-flex justify-content-end">
+                <Row>  
+                    <Col className="d-flex justify-content-end">
                     <Image src={require("../images/social-links.PNG")} className="img-fluid" alt="Portfolio Preview" />
                     </Col>
                 </Row>
