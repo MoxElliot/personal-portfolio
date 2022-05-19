6c01320819b78d594792cefeac7551a3a7199a03
Elliot Erickson <eric0952@gmail.com>
2022-05-19 12:44:58 -0700

    About Text & Picture
    Added headline, about me dummy text, and dummy horozontal profile pictue

--- src/routes/about-me.js
+++ src/routes/about-me.js
@@ -1,9 +1,30 @@
+import { Row, Col, Image, Container} from "react-bootstrap";
+
 export default function AboutMe() {
     return (
         <main style={{ padding: "1rem 0"}} >
-            <h2>
-                About Me
-            </h2>
+           <Container>
+                <Row>
+                    <Col className="text-center">
+                        <h2>Who is Elliot Erickson, really?</h2>
+                    </Col>
+                </Row>
+                <Row className="align-items-center">
+                    <Col md={4}>
+                        <p className="lead font-weight-bold">
+                        Mūsa, mihī causās memorā, quō nūmine laesō
+                        </p>
+                        <p className="lead font-weight-bold">Quidve dolēns, rēgīna deum tot volvere cāsūs
+                        īnsīgnem pietāte virum, tot adīre labōrēs impulerit.</p>
+                        <p className="lead font-weight-bold">Tantaene animīs caelestibus īrae?  Urbs antīqua fuit, Tyriī tenuēre colōnī. </p>   
+                        
+                    </Col>
+                    <Col md={8}>
+                        
+                        <Image src={require("../images/about-me-profile.PNG")} className="img-fluid" alt="Portfolio Preview" />
+                    </Col>
+                </Row>
+           </Container>
         </main>
     );
 }
