3811f1d66d91d0a2aa95dad809d89712ec64459c
Elliot Erickson <eric0952@gmail.com>
2022-06-03 09:31:41 -0700

    Font Awesome
    Added font awesome to the public folder (\font-awesome\all.js). Imported Github Icon

---
+++ src/components/SocialLinks.js
@@ -0,0 +1,14 @@
+import { Row, Col, Image } from "react-bootstrap"
+
+
+export const SocialLinks = () => {
+
+
+    return (
+        <Row>
+            <Col className="d-flex justify-content-end">
+                hello
+                <i className="fa-brands fa-github"></i>
+            </Col>
+        </Row>
+        )}
