93c07e3fc1fdcddbb3cbf3fb5dedbf57cc0e1635
Elliot Erickson <eric0952@gmail.com>
2022-06-04 10:16:20 -0700

    contact me format
    got the flextbox situationed with a d-flex container, justify-content-end, and text center on both the h2 and the col with the buttons

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -5,20 +5,51 @@ export const SocialLinks = () => {
 
 
     return (
-     <Container className="w-50 d-block text-center me-5 border border-secondary rounded ">
-         <h2>Contact Me</h2>
-        <Row >
-            <Col className="justify-content-end">
+     <Container  className="d-flex justify-content-end">
+        <Row className="border border-secondary rounded">
+            <h2 className="text-center">Contact Me</h2>
+       
+            <Col className="text-center" >
                 <ButtonGroup>
-                <Button href="https://www.freecodecamp.org/fccd6d2b58b-aecf-4f42-9e37-c12c7c081a35" target="_blank" variant="outline-secondary" className="border-0"><i className="fa-brands fa-free-code-camp fa-2x"></i></Button>  
+                <Button 
+                    href="https://www.freecodecamp.org/fccd6d2b58b-aecf-4f42-9e37-c12c7c081a35" 
+                    target="_blank" 
+                    variant="outline-secondary" 
+                    className="border-0">
+                        <i className="fa-brands fa-free-code-camp fa-2x"></i>
+                </Button>  
          
-                <Button href="discordapp.com/users/MoxElliot#8234" target="_blank" variant="outline-secondary" className="border-0"><i className="fa-brands fa-discord fa-2x"></i></Button> 
+                <Button 
+                    href="discordapp.com/users/MoxElliot#8234" 
+                    target="_blank" 
+                    variant="outline-secondary" 
+                    className="border-0">
+                        <i className="fa-brands fa-discord fa-2x"></i>
+                </Button> 
           
-                <Button href="https://github.com/MoxElliot" target="_blank" variant="outline-secondary" className="border-0"><i className="fa-brands fa-github fa-2x"></i></Button> 
+                <Button 
+                    href="https://github.com/MoxElliot" 
+                    target="_blank" 
+                    variant="outline-secondary" 
+                    className="border-0">
+                        <i className="fa-brands fa-github fa-2x"></i>
+                </Button> 
             
-                <Button href="mailto:eric0952@gmail.com" target="_blank" variant="outline-secondary" className="border-0"><i className="fa-solid fa-envelope fa-2x"></i></Button> 
+                <Button 
+                    href="mailto:eric0952@gmail.com" 
+                    target="_blank" 
+                    variant="outline-secondary" 
+                    className="border-0">
+                        <i className="fa-solid fa-envelope fa-2x"></i>
+                </Button> 
           
-                <Button href="https://www.linkedin.com/in/elliot-erickson-7a786822a/" target="_blank" variant="outline-secondary" className="border-0"><i className="fa-brands fa-linkedin fa-2x"></i></Button> 
+                <Button 
+                    href="https://www.linkedin.com/in/elliot-erickson-7a786822a/" 
+                    target="_blank" 
+                    variant="outline-secondary" 
+                    className="border-0">
+                        <i className="fa-brands fa-linkedin fa-2x"></i>
+                </Button> 
                 </ButtonGroup>
             </Col>
         </Row>
