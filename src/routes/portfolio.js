3bdbda702496d9ab9e7fe5ce1c1a0d023152ba0d
Elliot Erickson <eric0952@gmail.com>
2022-06-05 12:19:56 -0700

    Image and Card
    Added dummy image and caption with <Figure>, still need to learn responsive sizing via sass etc. Added Card for website details, and stack.

--- src/routes/portfolio.js
+++ src/routes/portfolio.js
@@ -1,9 +1,73 @@
+import '../index.sass';
+import {
+    Row, Col, Container,
+    Card,  Figure 
+        } from "react-bootstrap";
+
 export default function Portfolio() {
     return (
         <main style={{ padding: "1rem 0"}} >
-            <h2>
-                Portfolio
-            </h2>
+            <Container>
+                <Row className="text-center">
+                    <Col>
+                        <h2>Portfolio Details</h2>
+                        <h1> Test</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={6}>
+                        <a>
+                            <Figure>
+                                <Figure.Image 
+                                src={require("../images/media-tile.PNG")}
+                                width={400}
+                                height={400}
+                                />
+                                <Figure.Caption>
+                                    Portfolio Link #1
+                                </Figure.Caption>
+                            </Figure>
+                        </a>
+                    </Col>
+                    <Col md={6}>
+                        <Card>
+                            <Card.Header>
+                                Mūsa, mihī causās memorā, quō nūmine laesō.
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Title>
+                                    Features
+                                </Card.Title>
+                                <Card.Text>
+                                    <ul>
+                                        <li>
+                                            Vix e conspectu Siculae 
+                                        </li>
+                                        <li>
+                                            Telluris in altum
+                                        </li>
+                                        <li>
+                                            Vela dabant laeti, et spumas salis 
+                                        </li>
+                                        <li>
+                                            Aere ruebant cum Iuno
+                                        </li>
+                                        <li>
+                                            Aeternum servans sub pectore volnus
+                                        </li>
+                                    </ul>
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <Card.Title>
+                                    Technology Stack
+                                </Card.Title>
+                                <i className="fa-brands fa-react"></i>
+                            </Card.Footer>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </main>
     );
 }
