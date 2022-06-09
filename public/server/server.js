b0d8b1065cf7db07286714380d4eafb3b30113ff
Elliot Erickson <eric0952@gmail.com>
2022-06-09 13:05:13 -0700

    Start Here
    Start at the start here note tomorrow

--- public/server/server.js
+++ public/server/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const mongoose = require{"mongoose"}
+
 require("dotenv").config({ path: "./config.env" });
+
 const port = process.env.PORT || 5000;
+const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
+
+
+//Routes to use
+const usersRouter = require("./routes/users");
+app.use(require("/users"), usersRouter);
+
  
 app.listen(port, () => {
   // perform a database connection when server starts
@@ -17,3 +24,13 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
+
+const uri = "mongodb+srv://eric0952:Eric5042@mongoatlascluster.5nkuq.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("MongoDB Connected")
+})
+.catch(err => console.log(err))
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,18 +1,44 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { ReactDOM } from "react";
 
 export const ContactForm = () => {
+    const [inputs, setInputs] = useState({});
 
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert("Form Submited");
+    }
+{/*Start Here: https://medium.com/create-a-clocking-in-system-on-react/handle-form-submissions-in-react-to-a-mongodb-backend-a90cac7c81e9*/}
 return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 w-75">
             <Form.Label>Email Address</Form.Label>
-            <Form.Control type='email' placeholder="sample@company.com" />
+            <Form.Control 
+                type="email" 
+                name="email" 
+                nameplaceholder="sample@company.com" 
+                values={inputs.email || ""}
+                onChange={handleChange}
+            />
         </Form.Group>
         <Form.Group className="mb-3 w-75">
             <Form.Label>Message Me</Form.Label>
-            <Form.Control as="textarea" rows={3} placeholder="Your message here..."/>
+            <Form.Control 
+                as="textarea" 
+                name="message" 
+                rows={3} 
+                placeholder="Your message here..."
+                value={inputs.message || ""}
+                onChange={handleChange}    
+            />
         </Form.Group>
         <Button variant="secondary" type="submit">Submit</Button>
     </Form>
-
 )}
