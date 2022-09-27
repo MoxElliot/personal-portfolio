be57eeb5515f977b8863d5cac145e212ddc41088
Elliot Erickson <eric0952@gmail.com>
2022-09-27 14:26:27 -0700

    trying formkeep
    added formkeep data to form in ContactForm.js. commented out the handlesubmit

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -18,29 +18,36 @@ export const ContactForm = () => {
         alert("Thank you for your message!")
     }
 
-    async function handleSubmit(e){
-        e.preventDefault();
+    // async function handleSubmit(e){
+    //     e.preventDefault();
 
-        const newContact = { ...form };
+    //     const newContact = { ...form };
 
-        await fetch("http://localhost:5000/record/add", {
-            method: "POST",
-            headers: {
-                "Content-Type" : "application/json",
-            },
-            body: JSON.stringify(newContact),
-        })
-        .catch(error => {
-            window.alert(error);
-            return;
-        });
-        setform({ email: "", message: ""});
-        submitAlert();
-    }
+    //     await fetch("http://localhost:5000/record/add", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type" : "application/json",
+    //         },
+    //         body: JSON.stringify(newContact),
+    //     })
+    //     .catch(error => {
+    //         window.alert(error);
+    //         return;
+    //     });
+    //     setform({ email: "", message: ""});
+    //     submitAlert();
+    // }
 
    
 return (
-    <Form onSubmit={handleSubmit} className="border p-3">
+    <Form 
+        //onSubmit={handleSubmit} 
+        className="border p-3"
+        action="https:fomrkeep.com/f/5f7d494d8d88"
+        accept-charset="UTF-8"
+        enctype="multipart/form-data"
+        method="POST"
+        >
         <Form.Group className="mb-3 w-75">
             <Form.Label htmlFor="email">Email Address</Form.Label>
             <Form.Control 
@@ -48,7 +55,7 @@ return (
                 id="email" 
                 nameplaceholder="sample@company.com" 
                 value={form.email}
-                onChange={(e) => handleChange({ email: e.target.value })}
+               // onChange={(e) => handleChange({ email: e.target.value })}
             />
         </Form.Group>
         <Form.Group className="mb-3 w-75">
@@ -59,7 +66,7 @@ return (
                 rows={3} 
                 placeholder="Your message here..."
                 value={form.message}
-                onChange={(e) => handleChange({ message: e.target.value })}    
+               // onChange={(e) => handleChange({ message: e.target.value })}    
             />
         </Form.Group>
         <Button 
