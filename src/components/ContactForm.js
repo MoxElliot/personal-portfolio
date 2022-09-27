5a084f395690d1c1b844e9e87b2e787d1ccc8032
Elliot Erickson <eric0952@gmail.com>
2022-09-27 09:37:28 -0700

    back to localhost

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -23,7 +23,7 @@ export const ContactForm = () => {
 
         const newContact = { ...form };
 
-        await fetch("https://172.31.21.148:5000/record/add", {
+        await fetch("http://localhost:5000/record/add", {
             method: "POST",
             headers: {
                 "Content-Type" : "application/json",
