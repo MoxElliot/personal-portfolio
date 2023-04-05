74683b26d6417253cf91d71c97b52af0dcb23f4c
Elliot Erickson <elliot_erickson@protonmail.com>
2023-04-05 13:16:35 -0700

    fixed errors, accept-charset to acceptCharset, enctype to encType

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,52 +1,51 @@
-//import { useState } from "react";
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
-
 export const ContactForm = () => {
-    // const [form, setform] = useState({
-    //     email: "",
-    //     message:"",
-    // });
+    const [form, setform] = useState({
+        email: "",
+        message:"",
+    });
  
-    // const handleChange = (value) => {
-    //     return setform((prev) => {
-    //         return {...prev, ...value};
-    //     });
-    // }
+    const handleChange = (value) => {
+        return setform((prev) => {
+            return {...prev, ...value};
+        });
+    }
 
-    // const submitAlert = () => {
-    //     alert("Thank you for your message!")
-    // }
+    const submitAlert = () => {
+        alert("Thank you for your message!")
+    }
 
-    // async function handleSubmit(e){
-    //     e.preventDefault();
+    async function handleSubmit(e){
+        e.preventDefault();
 
-    //     const newContact = { ...form };
+        const newContact = { ...form };
 
-    //     await fetch("http://localhost:5000/record/add", {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type" : "application/json",
-    //         },
-    //         body: JSON.stringify(newContact),
-    //     })
-    //     .catch(error => {
-    //         window.alert(error);
-    //         return;
-    //     });
-    //     setform({ email: "", message: ""});
-    //     submitAlert();
-    // }
+        await fetch("http://localhost:5000/record/add", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify(newContact),
+        })
+        .catch(error => {
+            window.alert(error);
+            return;
+        });
+        setform({ email: "", message: ""});
+        submitAlert();
+    }
 
    
 return (
     <Form 
-        //onSubmit={handleSubmit} 
+        onSubmit={handleSubmit} 
         className="border p-3"
         target="_self"
-        action="https://formkeep.com/f/5f7d494d8d88"
-        accept-charset="UTF-8"
-        enctype="multipart/form-data"
+        //action="https://formkeep.com/f/5f7d494d8d88"
+        acceptCharset="UTF-8"
+        encType="multipart/form-data"
         method="POST"
         >
         <Form.Group className="mb-3 w-75">
@@ -55,8 +54,8 @@ return (
                 type="email" 
                 id="email" 
                 nameplaceholder="sample@company.com" 
-                //value={form.email}
-               // onChange={(e) => handleChange({ email: e.target.value })}
+                value={form.email}
+                onChange={(e) => handleChange({ email: e.target.value })}
             />
         </Form.Group>
         <Form.Group className="mb-3 w-75">
@@ -66,8 +65,8 @@ return (
                 id="message" 
                 rows={3} 
                 placeholder="Your message here..."
-                //value={form.message}
-               // onChange={(e) => handleChange({ message: e.target.value })}    
+                value={form.message}
+                onChange={(e) => handleChange({ message: e.target.value })}    
             />
         </Form.Group>
         <Button 
--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,4 +1,3 @@
-
 import {
     Row, Col, Button, 
     ButtonGroup, Container, Image, 
